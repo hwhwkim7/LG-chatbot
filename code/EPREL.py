ceaaa6cacdf7417a50928d2f4a2faf3e90527b24
HyeWon Kim <85098199+hwhwkim7@users.noreply.github.com>
2025-03-05 20:48:47 +0900

    Add files via upload

--- code/EPREL.py
+++ code/EPREL.py
@@ -1,121 +1,37 @@
-import time
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup
 import warnings
-import json
-
-
-def get_EPREL_DB():
-    warnings.filterwarnings("ignore")
-    page_num = 1
-    df1 = pd.DataFrame()
-    df2 = pd.DataFrame()
-    print("data 취합 중....")
-    while True:
-        url = 'https://eprel.ec.europa.eu/api/products/washingmachines2019?'
-        header = {
-            # 'accept' : 'application/json, text/plain, */*',
-            # 'accept-encoding' : 'gzip, deflate, br',
-            # 'accept-language ': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-            # 'cache-control' : 'No-Cache',
-            # 'content-type' : 'application/json',
-            # 'referer': 'https://eprel.ec.europa.eu/screen/product/washingmachines2019',
-            # 'sec-ch-ua-mobile' : '?0',
-            # 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-            'x-api-key': '3PR31D3F4ULTU1K3Y2020',
-            # 'x-requested-with' : 'XMLHttpRequest'
-        }
-        param = {'_page': str(page_num),
-                 '_limit': 100,
-                 'sort0': 'onMarketStartDateTS',
-                 'order0': 'DESC',
-                 'sort1': 'energyClass',
-                 'order1': 'DESC'}
-        r = requests.get(url, headers=header, params=param)
-        bs = BeautifulSoup(r.text, features="lxml")
-        df = pd.read_json(bs.text)
-        page = df['size'][0] // 100 + 1
-        df = pd.DataFrame(data=df.hits)
-        df1 = pd.concat((df1, df), axis=0)
-        # df1 = df1.append(df, ignore_index= True)
-        print(page_num, ' / ', page)
-        if page == page_num:
-            break
-        page_num += 1
-        # time.sleep(0.5)
-    print("항목 별 셀로 나누는 중")
-    for i in range(len(df1)):
-        df2[i] = pd.DataFrame(data=df1.iloc[i, 0].values(), index=df1.iloc[i, 0].keys())
-
-    return df2
-
-def get_EPREL_DB_v2(url_base,url_add):
-    warnings.filterwarnings("ignore")
-    page_num = 1
-    url = url_base + str(page_num) + url_add
-    df1 = pd.DataFrame()
-    df2 = pd.DataFrame()
-    print("data 취합 중....")
-    while True:
-        header = {'x-api-key': '3PR31D3F4ULTU1K3Y2020'}
-        r = requests.get(url, headers=header)
-        bs = BeautifulSoup(r.text, features="lxml")
-        df = pd.read_json(bs.text)
-        page = df['size'][0] // 100 + 1
-        df = pd.DataFrame(data=df.hits)
-        df1 = pd.concat((df1, df), axis=0)
-        # df1 = df1.append(df, ignore_index= True)
-        print(page_num, ' / ', page)
-        if page == page_num:
-            break
-        page_num += 1
-        # time.sleep(0.5)
-    print("항목 별 셀로 나누는 중")
-    for i in range(len(df1)):
-        df2[i] = pd.DataFrame(data=df1.iloc[i, 0].values(), index=df1.iloc[i, 0].keys())
-
-    return df2
 
 def get_EPREL_DB_v3():
     warnings.filterwarnings("ignore")
-    page_num = 1
-    url_WM_Base = 'https://eprel.ec.europa.eu/api/products/washingmachines2019?_page='
-    url_WD_Base = 'https://eprel.ec.europa.eu/api/products/washerdriers2019?_page='
-    url_TD_Base = 'https://eprel.ec.europa.eu/api/products/tumbledriers?_page='
-    url_WM_add = '&_limit=100&sort0=onMarketStartDateTS&order0=DESC&sort1=energyClass&order1=DESC'
-    url_WD_add = '&_limit=100&sort0=onMarketStartDateTS&order0=DESC&sort1=energyClassWash&order1=DESC'
-    url_TD_add = '&_limit=100&sort0=onMarketStartDateTS&order0=DESC&sort1=energyClass&order1=DESC'
+    page_num = 1 # 첫 번째 페이지부터 시작
 
-    df1 = pd.DataFrame()
-    df2 = pd.DataFrame()
-    print("data 취합 중....")
+    # 크롤링 하고자 하는 웹 페이지 주소
+    url_WM_Base = 'https://eprel.ec.europa.eu/api/products/washingmachines2019?_page='
+    # _limit=100: 한 번에 100개 제품을 불러옴
+    # 첫 번째 sort: 최신 출시 제품부터 (sort0=onMarketStartDateTS&order0=DESC)
+    # 두 번째 sort: 에너지 등급이 높은 제품부터
+    url_WM_add = '&_limit=100&sort0=onMarketStartDateTS&order0=DESC&sort1=energyClass&order1=DESC'
+
+    df_list = [] # 여러 page에서 얻은 dataframe을 저장하는 list
     while True:
+        # EPREL 사이트에서 페이지를 하나씩 넘겨가면서 원하는 순서대로 정렬
         url = url_WM_Base + str(page_num) + url_WM_add
         header = {'x-api-key': '3PR31D3F4ULTU1K3Y2020'}
         r = requests.get(url, headers=header)
-        bs = BeautifulSoup(r.text, features="lxml")
-        df = pd.read_json(bs.text)
-
-        data = json.loads(bs.text)
-
-        # 예쁘게 출력
+        # 해당 페이지에서 데이터를 가져와서 json 형태로 변환
+        df = pd.read_json(r.text)
         page = df['size'][0] // 100 + 1
         df = pd.DataFrame(data=df.hits)
-        df1 = pd.concat((df1, df), axis=0)
-        # df1 = df1.append(df, ignore_index= True)
-        print(page_num, ' / ', page)
-        if page == page_num:
+        df_list.append(df)
+
+        if page == page_num: # 더이상 넘길 페이지가 없으면 종료
             break
         page_num += 1
-        # time.sleep(0.5)
-    print("항목 별 셀로 나누는 중")
-    for i in range(len(df1)):
-        df2[i] = pd.DataFrame(data=df1.iloc[i, 0].values(), index=df1.iloc[i, 0].keys())
 
+    # json 형태로 구성된 상태
+    df1 = pd.concat(df_list, axis=0).reset_index(drop=True)
+    # json을 개별 컬럼 형태로 변환
+    structured_data = [pd.DataFrame(data=row.values(), index=row.keys()).T for row in df1.iloc[:, 0]]
+    df2 = pd.concat(structured_data, ignore_index=True)
     return df2
-
-
-
-
-
--- code/data.py
+++ code/data.py
@@ -1,5 +1,4 @@
 from EPREL import *
-import time
 import pre_functions
 import argparse
 import os
@@ -9,35 +8,43 @@ parser.add_argument('--craw', default='all')
 parser.add_argument('--method', default='filter')
 args = parser.parse_args()
 
+# 크롤링만 진행
 if args.craw == 'T':
-    start_time = time.time()
-
+    # EPREL.py의 get_EPREL_DB_v3() 함수 호출하여 크롤링 진행
     df = get_EPREL_DB_v3()
+    # 크롤링으로 얻은 dataframe csv로 저장
     df.T.to_csv('../data/data_original.csv', index=False)
 
-    end_time = time.time()
-    elapse_time = end_time - start_time
-    print(f"Crawling Time : {elapse_time} sec")
-
+# 전처리만 진행
 elif args.craw == 'F':
+    # 이미 저장했던 크롤링 csv data 가져오기
     df = pd.read_csv('../data/data_original.csv')
 
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.dirname(current_dir)
-    output_path = os.path.join(parent_dir, "output")
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    # output 폴더가 있는지 확인하고 없으면 폴더를 생성
+    # (전처리한 data가 output 폴더 내에 저장되는데, 해당 폴더가 없으면 오류나기 때문에 이런 과정을 진행)
+    current_dir = os.path.dirname(os.path.abspath(__file__)) # 현재 파일 경로 (data.py 파일의 위치 파악)
+    parent_dir = os.path.dirname(current_dir) # 현재 파일의 부모 경로 (data.py 파일이 들어있는 code 폴더의 위치 파악)
+    output_path = os.path.join(parent_dir, "output") # 부모 경로에서의 output 폴더 존재 확인 (code 폴더와 같은 위치에 output 폴더가 있는지 확인)
+    if not os.path.exists(output_path): # output 폴더 없으면
+        os.makedirs(output_path) # 폴더 생성
 
+    # 전처리 진행
     pre_functions.pre_process(df, args.method)
 
+# 크롤링 + 전처리
 elif args.craw == 'all':
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.dirname(current_dir)
-    output_path = os.path.join(parent_dir, "output")
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    # output 폴더가 있는지 확인하고 없으면 폴더를 생성
+    # (전처리한 data가 output 폴더 내에 저장되는데, 해당 폴더가 없으면 오류나기 때문에 이런 과정을 진행)
+    current_dir = os.path.dirname(os.path.abspath(__file__)) # 현재 파일 경로 (data.py 파일의 위치 파악)
+    parent_dir = os.path.dirname(current_dir) # 현재 파일의 부모 경로 (data.py 파일이 들어있는 code 폴더의 위치 파악)
+    output_path = os.path.join(parent_dir, "output") # 부모 경로에서의 output 폴더 존재 확인 (code 폴더와 같은 위치에 output 폴더가 있는지 확인)
+    if not os.path.exists(output_path): # output 폴더 없으면
+        os.makedirs(output_path) # 폴더 생성
 
+    # EPREL.py의 get_EPREL_DB_v3() 함수 호출하여 크롤링 진행
     df = get_EPREL_DB_v3()
+    # 크롤링으로 얻은 dataframe csv로 저장
     df.T.to_csv('../data/data_original.csv', index=False)
 
+    # 전처리 진행
     pre_functions.pre_process(df.T, 'IF', "../output/pre_data_")
