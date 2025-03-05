481177a02fdea1fbd772d3697be20dfa940224db
HyeWon Kim <85098199+hwhwkim7@users.noreply.github.com>
2025-03-05 14:53:33 +0900

    Update data.py

--- code/data.py
+++ code/data.py
@@ -2,9 +2,10 @@ from EPREL import *
 import time
 import pre_functions
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--craw', default='F')
+parser.add_argument('--craw', default='all')
 parser.add_argument('--method', default='filter')
 args = parser.parse_args()
 
@@ -21,4 +22,22 @@ if args.craw == 'T':
 elif args.craw == 'F':
     df = pd.read_csv('../data/data_original.csv')
 
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    output_path = os.path.join(parent_dir, "output")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
     pre_functions.pre_process(df, args.method)
+
+elif args.craw == 'all':
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    output_path = os.path.join(parent_dir, "output")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    df = get_EPREL_DB_v3()
+    df.T.to_csv('../data/data_original.csv', index=False)
+
+    pre_functions.pre_process(df.T, 'IF', "../output/pre_data_")
